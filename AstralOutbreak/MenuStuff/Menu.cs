08e3c4f6a84c31593042582c401a32ca3b6d80ae
nat7611 <nat7611@GDD-8>
2017-02-24 15:45:48 -0500

    Menu skeleton and MenuButton

---
+++ AstralOutbreak/MenuStuff/Menu.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstralOutbreak
+{
+    enum ActiveMenu { MainMenu, PauseMenu, OptionsMenu, GameOverMenu} //Passed in during Menu's constructor to determine what gets loaded
+
+    class Menu
+    {
+        public List<MenuContent> items { get; private set; } //List of things to draw 
+
+        public Menu()
+        {
+            Load(ActiveMenu.MainMenu);
+        }
+
+        public void Load(ActiveMenu type)
+        {
+            switch (type)
+            {
+                case (ActiveMenu.MainMenu):
+                    {
+                        LoadMain();
+                        break;
+                    }
+                case (ActiveMenu.PauseMenu):
+                    {
+                        LoadPause();
+                        break;
+                    }
+
+                case (ActiveMenu.OptionsMenu):
+                    {
+                        LoadOptions();
+                        break;
+                    }
+                case (ActiveMenu.GameOverMenu):
+                    {
+                        LoadGameOver();
+                        break;
+                    }
+            }
+        }
+
+        //Load options
+        private void LoadMain() //Loads main menu assets
+        {
+
+        }
+        private void LoadPause() //Loads main menu assets
+        {
+
+        }
+        private void LoadOptions() //Loads main menu assets
+        {
+
+        }
+        private void LoadGameOver() //Loads main menu assets
+        {
+
+        }
+    }
+}
---
+++ AstralOutbreak/MenuStuff/MenuButton.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstralOutbreak
+{
+    class MenuButton : MenuContent
+    {
+        //Variables
+        private Rectangle hitbox;
+        public Rectangle Hitbox
+        {
+            get { return hitbox; }
+        }
+        public new Point Location
+        {
+            get { return hitbox.Location; }
+            private set { hitbox.Location = value; }
+        }
+
+        //Constructor
+        public MenuButton(int x, int y, int width, int height, Texture2D texture) : base(x, y, texture)
+        {
+            hitbox = new Rectangle(base.Location, new Point(width, height));
+        }
+
+        
+    }
+}
