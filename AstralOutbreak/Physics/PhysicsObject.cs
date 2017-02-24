ff3ef9a8faec17598d83f701a3a797ba43e98e0e
Markadelf <Markadelf@LAPTOP-LG3TV9F0>
2017-02-24 17:32:38 -0500

    Physics stuff

--- AstralOutbreak/Physics/PhysicsObject.cs
+++ AstralOutbreak/Physics/PhysicsObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,87 @@ using System.Threading.Tasks;
 
 namespace AstralOutbreak
 {
-    class PhysicsObject
+    /// <summary>
+    /// The most basic Object that handles the physics in the game.
+    /// </summary>
+    public class PhysicsObject
     {
-        Vector2
+        //Motion
+        public bool Mobile { get; set; }
+        public Vector Position { get; set; }
+        public Vector VelocityX { get; set; }
+        public Vector VelocityY { get; set; }
+        public Vector Velocity
+        {
+            get
+            {
+                return VelocityX + VelocityY;
+            }
+        }
+        public Vector Acceleration { get; set; }
+
+        //Rectangle parameters
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        /// <summary>
+        /// Creates a physics object with everything it needs.
+        /// </summary>
+        /// <param name="pos">Top left corner.</param>
+        /// <param name="width">Width of the hitbox.</param>
+        /// <param name="height">Height of the hitbox.</param>
+        /// <param name="mobile">Can the object move.</param>
+        public PhysicsObject(Vector2 pos, float width, float height, bool mobile = false)
+        {
+            Mobile = mobile;
+            Position = pos;
+            VelocityX = new Vector2(0, 0);
+            VelocityY = new Vector2(0, 0);
+
+
+        }
+
+        public void UpdateVelocity(float time)
+        {
+            VelocityX.X += Acceleration.X * time;
+        }
+
+        /// <summary>
+        /// Checks for a collision between two objects.
+        /// </summary>
+        /// <param name="other">Object to check collisions against.</param>
+        /// <returns>True if there is a collision.</returns>
+        public bool CheckCollision(PhysicsObject other)
+        {
+            if (Position.X > other.Position.X + other.Width)
+                return false;
+            if (Position.X + Width < other.Position.X)
+                return false;
+            if (Position.Y > other.Position.Y + other.Height)
+                return false;
+            if (Position.Y + Height < other.Position.Y)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Checks for a collision between two objects after a movement.
+        /// </summary>
+        /// <param name="other">Object to check collisions against.</param>
+        /// <param name="vel">Velocity of movement.</param>
+        /// <returns>True if there is a collision.</returns>
+        public bool CheckCollision(PhysicsObject other, Vector2 vel)
+        {
+            if (Position.X + vel.X > other.Position.X + other.Width)
+                return false;
+            if (Position.X + Width + vel.X < other.Position.X)
+                return false;
+            if (Position.Y + vel.Y > other.Position.Y + other.Height)
+                return false;
+            if (Position.Y + Height + vel.Y < other.Position.Y)
+                return false;
+            return true;
+        }
+
     }
 }
