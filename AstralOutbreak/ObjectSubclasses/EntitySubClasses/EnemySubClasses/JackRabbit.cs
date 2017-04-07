e28b049c954d3945cc2ab3eefcb435a7a1967dfe
mvh9602 <mvh9602@NM2550-23>
2017-04-07 09:37:45 -0400

    fixed namespaces

--- AstralOutbreak/ObjectSubclasses/EntitySubClasses/EnemySubClasses/JackRabbit.cs
+++ AstralOutbreak/ObjectSubclasses/EntitySubClasses/EnemySubClasses/JackRabbit.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AstralOutbreak.ObjectSubclasses.EntitySubClasses.EnemySubClasses
+namespace AstralOutbreak
 {
     public enum JackRabbitState { MovingLeft, MovingRight, Jumping, Falling, ShootingRight, ShootingLeft }
 
--- AstralOutbreak/ObjectSubclasses/EntitySubClasses/EnemySubClasses/Slug.cs
+++ AstralOutbreak/ObjectSubclasses/EntitySubClasses/EnemySubClasses/Slug.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AstralOutbreak.ObjectSubclasses.EntitySubClasses.EnemySubClasses
+namespace AstralOutbreak
 {
     public enum SlugState { MovingRight, MovingLeft, Falling }
 
