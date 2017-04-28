d89238b09eeaab8e2d2ace7c00528a05b4c6f993
mxd5700 <mxd5700@NM2550-23>
2017-04-28 15:51:43 -0400

    Bug fix - Items - Boss

--- AstralOutbreak/ObjectSubclasses/EntitySubClasses/EnemySubClasses/Bosses/SwarmMob.cs
+++ AstralOutbreak/ObjectSubclasses/EntitySubClasses/EnemySubClasses/Bosses/SwarmMob.cs
@@ -59,10 +59,10 @@ namespace AstralOutbreak
         }
 
 
-        public SwarmMob(Vector2 pos, float width, float height, float health, float damage, bool mobile = true) : base(pos, width, height, health, damage, mobile)
+        public SwarmMob(Vector2 pos, float width, float height, float health, float damage, bool mobile = true) : base(pos, width, height, health, 1, mobile)
         {
             Gravity = false;
-            MaxVelocity = new Vector(200, 200);
+            MaxVelocity = new Vector(300, 300);
             MySwarm.Mobs.Add(this);
         }
 
@@ -97,8 +97,14 @@ namespace AstralOutbreak
                     MySwarm.Activate();
                     Target = (MySwarm.GetCenter() + RoomManager.Active.PlayerOne.Center) / 2;
                     List<PhysicsObject> swarmMobs = new List<PhysicsObject>();
-                    for(int i = 0; i < 50; i++)
-                        swarmMobs.Add(new SwarmMob(this));
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            swarmMobs.Add(new SwarmMob(this));
+                            swarmMobs[5 * i + j].Position += new Vector(2*i, 2*j);
+                        }
+                    }
                     RoomManager.Active.AddEntities(swarmMobs);
                 }
 
